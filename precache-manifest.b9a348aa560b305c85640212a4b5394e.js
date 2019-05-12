2a484d0a017a0383efc6aa13f7bc7a564c822ce5
Gabby Campos <gabbycampos@Gabbys-MacBook-Pro.local>
2019-05-11 19:54:06 -0500

    Updates

---
+++ precache-manifest.b9a348aa560b305c85640212a4b5394e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7573de3d15ad506e799a9f8c1ef9842",
+    "url": "/gemstore/index.html"
+  },
+  {
+    "revision": "c3009c985cb82b6e766c",
+    "url": "/gemstore/static/css/main.9c08f8e5.chunk.css"
+  },
+  {
+    "revision": "2a067f888427b7e2d845",
+    "url": "/gemstore/static/js/2.6b18c407.chunk.js"
+  },
+  {
+    "revision": "c3009c985cb82b6e766c",
+    "url": "/gemstore/static/js/main.c684cef2.chunk.js"
+  },
+  {
+    "revision": "eba16b01b14369966908",
+    "url": "/gemstore/static/js/runtime~main.f59a08c2.js"
+  }
+]);
